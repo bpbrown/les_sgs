3acb8e4d8f1bbd65d2ff7ef0dd3e25ceec2f1652
Benjamin Brown <bpbrown@gmail.com>
2023-02-19 17:35:50 -0700

    Adds plot_scalar from rb_d3

--- plot_scalar.py
+++ plot_scalar.py
@@ -1,6 +1,5 @@
 """
-Plot scalar outputs from scalar output .h5 file.
-Assumes all scalar output is in a single file.
+Plot scalar outputs from scalar_output.h5 file.
 
 Usage:
     plot_scalar.py <file> [options]
@@ -31,16 +30,12 @@ else:
     data_dir += '/'
     output_path = pathlib.Path(data_dir).absolute()
 
-fig_KE, ax_KE = plt.subplots(nrows=2)
-fig_Re, ax_Re = plt.subplots(nrows=2)
-fig_ens, ax_ens = plt.subplots(nrows=2)
-fig_Nu, ax_Nu = plt.subplots(nrows=2)
-
 f = h5py.File(file, 'r')
 data = {}
 t = f['scales/sim_time'][:]
+data_slice = (slice(None),0,0) #2-d slicing; TODO: fix for 3-d too.
 for key in f['tasks']:
-    data[key] = f['tasks/'+key][:,0,0,0]
+    data[key] = f['tasks/'+key][data_slice]
 f.close()
 
 
@@ -53,32 +48,75 @@ if args['--times']:
 else:
     subrange = False
 
-ax_KE[0].plot(t, data['KE'], label='KE')
-ax_KE[1].plot(t, data['KE'], label='KE')
-ax_KE[1].set_xlabel('t')
-ax_KE[1].set_ylabel('KE')
-ax_KE[1].set_yscale('log')
+if not 'τp' in data:
+    data['τp']=data['τb1']*np.NAN
 
-ax_Re[0].plot(t, data['Re'], label='Re')
-ax_Re[1].plot(t, data['Re'], label='Re')
-ax_Re[1].set_xlabel('t')
-ax_Re[1].set_ylabel('Re')
-ax_Re[1].set_yscale('log')
+energy_keys = ['KE', 'PE']
 
-ax_ens[0].plot(t, data['enstrophy'], label='ens')
-ax_ens[1].plot(t, data['enstrophy'], label='ens')
-ax_ens[1].set_xlabel('t')
-ax_ens[1].set_ylabel(r'$\omega^2$')
-ax_ens[1].set_yscale('log')
+fig_E, ax_E = plt.subplots(nrows=2)
+for key in energy_keys:
+    ax_E[0].plot(t, data[key], label=key)
+for key in energy_keys[:-1]:
+    ax_E[1].plot(t, data[key], label=key)
 
-fig_KE.savefig('{:s}/energies.pdf'.format(str(output_path)))
-fig_Re.savefig('{:s}/Reynolds.pdf'.format(str(output_path)))
-fig_ens.savefig('{:s}/enstrophy.pdf'.format(str(output_path)))
+for ax in ax_E:
+    if subrange:
+        ax.set_xlim(t_min,t_max)
+    ax.set_xlabel('time')
+    ax.set_ylabel('energy density')
+    ax.legend(loc='lower left')
+fig_E.savefig('{:s}/energies.pdf'.format(str(output_path)))
+for ax in ax_E:
+    ax.set_yscale('log')
+fig_E.savefig('{:s}/log_energies.pdf'.format(str(output_path)))
 
-if 'Nu' in data:
-    ax_ens[0].plot(t, data['enstrophy'], label='ens')
-    ax_ens[1].plot(t, data['enstrophy'], label='ens')
-    ax_ens[1].set_xlabel('t')
-    ax_ens[1].set_ylabel(r'$\omega^2$')
-    ax_ens[1].set_yscale('log')
-    fig_Nu.savefig('{:s}/Nusselt.pdf'.format(str(output_path)))
+fig_tau, ax_tau = plt.subplots(nrows=2)
+ax_tau[0].plot(t, data['τu1'], label=r'$\tau_{u,1}$')
+ax_tau[0].plot(t, data['τu2'], label=r'$\tau_{u,2}$')
+ax_tau[0].plot(t, data['τb1'], label=r'$\tau_{b,1}$')
+ax_tau[0].plot(t, data['τb2'], label=r'$\tau_{b,2}$')
+ax_tau[0].plot(t, data['τp'], label=r'$\tau_{p}$')
+ax_tau[1].plot(t, data['τu1'], label=r'$\tau_{u,1}$')
+ax_tau[1].plot(t, data['τu2'], label=r'$\tau_{u,2}$')
+ax_tau[1].plot(t, data['τb1'], label=r'$\tau_{b,1}$')
+ax_tau[1].plot(t, data['τb2'], label=r'$\tau_{b,2}$')
+ax_tau[1].plot(t, data['τp'], label=r'$\tau_{p}$')
+for ax in ax_tau:
+    if subrange:
+        ax.set_xlim(t_min,t_max)
+    ax.set_xlabel('time')
+    ax.set_ylabel(r'$<\tau>$')
+    ax.legend(loc='lower left')
+ax_tau[1].set_yscale('log')
+ylims = ax_tau[1].get_ylim()
+ax_tau[1].set_ylim(max(1e-14, ylims[0]), ylims[1])
+fig_tau.savefig('{:s}/tau_error.pdf'.format(str(output_path)))
+
+fig_f, ax_f = plt.subplots(nrows=2)
+for ax in ax_f:
+    ax.plot(t, data['Re'], label='Re')
+    ax_r = ax.twinx()
+    ax_r.plot(t, data['Nu'], label='Nu', color='tab:orange')
+    if subrange:
+        ax.set_xlim(t_min,t_max)
+    ax.set_xlabel('time')
+    ax.set_ylabel('fluid parameters')
+    ax.legend(loc='lower left')
+
+ax_f[1].set_yscale('log')
+ax_r.set_yscale('log') # relies on it being the last instance; poor practice
+
+fig_f.savefig('{:s}/Re_and_Nu.pdf'.format(str(output_path)))
+
+
+data['τu'] = np.maximum(data['τu1'], data['τu2'])
+data['τb'] = np.maximum(data['τb1'], data['τb2'])
+
+benchmark_set = ['KE', 'Re', 'PE', 'Nu', 'τu', 'τb', 'τu1', 'τb1', 'τu2', 'τb2','τp']
+i_ten = int(0.9*data[benchmark_set[0]].shape[0])
+for benchmark in benchmark_set:
+    print("{:s} benchmark {:14.12g} +- {:4.2g} (averaged from {:g}-{:g})".format(benchmark, np.mean(data[benchmark][i_ten:]), np.std(data[benchmark][i_ten:]), t[i_ten], t[-1]))
+# print(40*'-')
+# for benchmark in benchmark_set:
+#     print("{:s} benchmark {:14.12g} (at t={:g})".format(benchmark, data[benchmark][-1], t[-1]))
+print("total simulation time {:6.2g}".format(t[-1]-t[0]))
